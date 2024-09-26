5ab93b6b30a12a615cb694d2c23eefe03f558edc
unknown <lautarorcloudy@gmail.com>
2024-09-26 17:29:52 -0300

    update

--- src/Helpers/Global.js
+++ src/Helpers/Global.js
@@ -1,6 +1,6 @@
 const Global =  {
     // url: "http://localhost:3000/api/"
-    url:"https://nnbbsociedades-back-lmhlqneac-lautarocloudys-projects.vercel.app/api/"
+    url:"https://nnbbsociedades-back.vercel.app/api/"
    }
    
    export default Global
